8e85e930bc20a3757219b022270c8fbeba068ef4
Daniel Ebert <iuq@web.de>
2023-09-24 03:31:59 -0700

    electron rect tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content : [
+        './src/renderer/**/*.{js,jsx,ts,tsx}',
+        './node_modules/react-tailwindcss-datepicker/dist/index.esm.js'
+    ],
+    theme : {},
+    variants : {},
+    plugins : [],
+};
